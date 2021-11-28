983d5d6bbf635dbea5a800811ced1020b4308e82
Kyle Troutner <kyletroutner1@gmail.com>
2021-11-28 19:09:20 +0900

    Add score input to match page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,8 +2,29 @@
 
 # Controller for matches
 class MatchesController < ApplicationController
+  before_action :find_match, only: %i(show edit update)
+
   # GET /matches/:id
   def show
+  end
+
+  # GET /matches/:id/edit
+  def edit
+  end
+
+  # PATCH /matches/:id/edit
+  def update
+    @match.update!(score_params)
+    redirect_to @match
+  end
+
+  private
+
+  def find_match
     @match = Match.find(params[:id])
+  end
+
+  def score_params
+    params.require(:match).permit(:home_score, :away_score)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get 'home', to: 'home#index'
-  get 'competitions', to: 'competitions#index'
-  get 'competitions/:id', to: 'competitions#show'
-  get 'seasons/:id', to: 'seasons#show'
-  get 'fixtures/:id', to: 'fixtures#show'
-  get 'matches/:id', to: 'matches#show'
+  get 'home', to: 'home#index', as: :home
+  get 'competitions', to: 'competitions#index', as: :competitions
+  get 'competitions/:id', to: 'competitions#show', as: :competition
+  get 'seasons/:id', to: 'seasons#show', as: :season
+  get 'fixtures/:id', to: 'fixtures#show', as: :fixture
+  get 'matches/:id', to: 'matches#show', as: :match
+  get 'matches/:id/edit', to: 'matches#edit', as: :edit_match
+  patch 'matches/:id', to: 'matches#update', as: :update_match
 end
