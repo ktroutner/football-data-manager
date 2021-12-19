21d266b40385bac2dda0ba72084d9ace20653c0a
Kyle Troutner <kyletroutner1@gmail.com>
2021-12-19 21:38:43 +0900

    Add competition results page

---
+++ test/controllers/competitions/results_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Competitions
+  class ResultsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
